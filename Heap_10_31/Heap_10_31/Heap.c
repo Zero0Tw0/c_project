3ddc8afc36ccdb0c9ddcfab3597b689acdec728f
Zero0Tw0 <1464001349@qq.com>
2021-11-01 16:32:54 +0800

    大堆10_31

--- Heap_10_31/Heap_10_31/Heap.c
+++ Heap_10_31/Heap_10_31/Heap.c
@@ -2,82 +2,95 @@
 
 #include"Heap.h"
 
-void Swap(HeapDataType* a, HeapDataType* b)
+//堆操作
+//
+void Swap(HeapDataType* a, HeapDataType* b)			//交换函数
 {
-	HeapDataType* tmp = *a;
+	assert(a);
+	assert(b);
+	HeapDataType* tmp = *a;						
 	*a = *b;
 	*b = tmp;
 	return;
 }
 
-void AdjustDown(Heap* ph, int parent)
+void AdjustDown(Heap* ph, int parent)				//向下调整算法
 {
+	assert(ph);
 	int	child = parent * 2 + 1;
 	while (child < ph->size)
 	{
-		if (child + 1 < ph->size && ph->data[child + 1] > ph->data[child])
+		if (child + 1 < ph->size && ph->data[child + 1] > ph->data[child])	
 		{
-			child++;
+			child++;		//找出左右孩子中大的那个
 		}
 
-		if (ph->data[child] > ph->data[parent])
+		if (ph->data[child] > ph->data[parent])			//孩子大于父亲就交换
 		{
 			Swap(&ph->data[child], &ph->data[parent]);
-			parent = child;
-			child = parent * 2 + 1;
+			parent = child;								//孩子成为父亲
+			child = parent * 2 + 1;						//继续找孩子
 		}
 		else
 		{
-			break;
+			break;										//孩子不大于父亲结束调整
 		}
 	}
 }
 
-void AdjustUp(Heap* ph, int child)
-{
-	int parent = (child - 1) / 2;
-	while (child > parent)
-	{
-		if (ph->data[child] > ph->data[parent])
-		{
-			Swap(&ph->data[child], &ph->data[parent]);
-			child = parent;
-			parent = (child - 1) / 2;
-		}
-		else
-		{
-			break;
-		}
-	}
-
-}
-
-void BuildHeap(Heap* ph)
+void AdjustUp(Heap* ph, int child)					//向上调整算法
 {
 	assert(ph);
-	for (int i = (ph->size - 1 - 1) / 2; i >= 0; i--)
+	assert(child < ph->size);
+	int parent = (child - 1) / 2;					//找出父亲
+	while (child > parent)
 	{
-		AdjustDown(ph, i);
+		if (ph->data[child] > ph->data[parent])		//孩子大于父亲交换
+		{
+			Swap(&ph->data[child], &ph->data[parent]);
+			child = parent;							//父亲成为孩子
+			parent = (child - 1) / 2;				//继续找父亲
+		}
+		else
+		{
+			break;									//孩子不大于父亲结束调整
+		}
+	}
+
+}
+
+void BuildHeap(Heap* ph)							//建堆
+{
+	assert(ph);
+	for (int i = (ph->size - 1 - 1) / 2; i >= 0; i--)	
+	{
+		AdjustDown(ph, i);							//从最后一个父亲想前调整
 	}
 }
 
 
+//堆接口
+//
 void HeapInit(Heap* ph, HeapDataType* str, int size)	//堆初始
 {
+	assert(ph);
+	assert(str);
+	//给定数组建成堆，并初始
 	HeapDataType* DataSpace;
-	DataSpace = (HeapDataType*)malloc(sizeof(HeapDataType) * size);
+	DataSpace = (HeapDataType*)malloc(sizeof(HeapDataType) * size);	//开辟给定数组相等空间
 	assert(DataSpace);
-	memcpy(DataSpace, str, size*sizeof(HeapDataType));
-	ph->data = DataSpace;
-	ph->size = size;
+	memcpy(DataSpace, str, size*sizeof(HeapDataType));				//将数组内容拷贝进堆
+	ph->data = DataSpace;											
+	ph->size = size;												//堆初始大小等于数组大小
 	ph->capacity = size;
 
 	//建堆
-	BuildHeap(ph);
+	BuildHeap(ph);												
 }
 
 void HeapPrint(Heap* ph)								//堆打印
 {
+	assert(ph);
 	for (int i = 0; i < ph->size; i++)
 	{
 		printf("%d ", ph->data[i]);
@@ -87,7 +100,7 @@ void HeapPrint(Heap* ph)								//堆打印
 void HeapPush(Heap* ph, HeapDataType x)					//入堆
 {
 	assert(ph);
-	if (ph->capacity == ph->size)
+	if (ph->capacity == ph->size)				//判段堆是否已满
 	{
 		HeapDataType* newspace;
 		newspace = (HeapDataType*)realloc(ph->data, sizeof(HeapDataType) * (ph->size)*2);
@@ -98,17 +111,38 @@ void HeapPush(Heap* ph, HeapDataType x)					//入堆
 	ph->data[ph->size] = x;
 	ph->size++;
 
-	AdjustUp(ph,ph->size - 1);
+	AdjustUp(ph,ph->size - 1);					//将入堆数据放到堆最后，进行向上调整
 }
 
-void HeapPop(Heap* ph);									//出堆
-HeapDataType HeapTop(Heap* ph);							//堆顶数据
-int HeapSize(Heap* ph);									//堆大小
-bool HeapEmpty(Heap* ph);								//判空
+void HeapPop(Heap* ph)									//出堆
+{
+	//出堆头数据
+	assert(ph);
+	Swap(&ph->data[0], &ph->data[ph->size - 1]);		//将头尾数据交换
+	ph->size--;											//堆大小减1
+	AdjustDown(ph, 0);									//堆剩余内容向下调整
+}
+
+HeapDataType HeapTop(Heap* ph)							//堆顶数据
+{
+	assert(ph);
+	assert(ph->data);
+	return ph->data[0];
+}
+int HeapSize(Heap* ph)									//堆大小
+{
+	assert(ph);
+	return ph->size;
+}
+bool HeapEmpty(Heap* ph)								//判空
+{
+	assert(ph);
+	return ph->size == 0;					 
+}
 void HeapDestroy(Heap* ph)								//堆销毁
 {
 	assert(ph);
-	free(ph->data);
-	ph->data = NULL;
-	ph->size = ph->capacity = 0;
+	free(ph->data);							//释放堆空间
+	ph->data = NULL;						//堆空间指针指空
+	ph->size = ph->capacity = 0;			//堆大小堆容量置零
 }
--- Heap_10_31/Heap_10_31/test.c
+++ Heap_10_31/Heap_10_31/test.c
@@ -7,13 +7,23 @@ void test1()
 	HeapDataType str[] = { 27,15,19,18,28,34,65,49,25,37 };
 	Heap heap;
 	int strsize = sizeof(str) / sizeof(str[0]);
-	HeapInit(&heap, str, strsize);
+	HeapInit(&heap, str, strsize);			//¶Ñ³õÊ¼²âÊÔ
+	HeapPrint(&heap);						//¶Ñ´òÓ¡²âÊÔ
+
+	HeapPush(&heap, 88);					//Èë¶Ñ²âÊÔ
 	HeapPrint(&heap);
 
-	HeapPush(&heap, 88);
+	while (!HeapEmpty(&heap))				//ÅĞ¿Õ²âÊÔ
+	{
+		printf("top:%d\n", HeapTop(&heap));	//¶Ñ¶¥Êı¾İ²âÊÔ
+		HeapPop(&heap);						//³ö¶Ñ²âÊÔ
+		HeapPrint(&heap);		
+	}
+
+	HeapPush(&heap, 88);					//³ö¿ÕÔÙÈë²âÊÔ
 	HeapPrint(&heap);
 
-	HeapDestroy(&heap);
+	HeapDestroy(&heap);						//¶ÑÏú»Ù²âÊÔ
 
 }
 
